61d44386b36c5855c83e6226d05b42dbc1f23bfa
de Leon, Eduardo <eduardo.de.leon@accenture.com>
2018-06-07 09:08:24 -0500

    Added stylesheet and index. Configurated bundler and added it to DOM.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+
+import config from '../webpack.config.dev';
 
 const port = 3333,
-      app = express();
+      app = express(),
+      compiler = webpack(config);
+
+// Express uses `webpack-dev-middleware` with our `compiler`
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
